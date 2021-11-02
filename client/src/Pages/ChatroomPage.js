ca49872ab518a2c687817c8ba0dd84cefa360d9b
MalekWahmi <wahmimalek@gmail.com>
2021-11-02 23:09:01 +0100

    real chat with auth

---
+++ client/src/Pages/ChatroomPage.js
@@ -0,0 +1,94 @@
+import React ,{useState,useEffect ,useRef} from "react";
+
+import { withRouter } from "react-router-dom";
+
+const ChatroomPage = ({ match, socket }) => {
+  const chatroomId = match.params.id;
+  const [messages, setMessages] = useState([]);
+  const messageRef = useRef();
+  const [userId, setUserId] = useState("");
+
+  const sendMessage = () => {
+    if (socket) {
+      socket.emit("chatroomMessage", {
+        chatroomId,
+        message: messageRef.current.value,
+      });
+
+      messageRef.current.value = "";
+    }
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("CC_Token");
+    if (token) {
+      const payload = JSON.parse(atob(token.split(".")[1]));
+      setUserId(payload.id);
+    }
+    if (socket) {
+      socket.on("newMessage", (message) => {
+        const newMessages = [...messages, message];
+        setMessages(newMessages);
+      });
+    }
+    //eslint-disable-next-line
+  }, [messages]);
+  console.log(messages);
+  
+  useEffect(() => {
+    if (socket) {
+      socket.emit("joinRoom", {
+        chatroomId,
+      });
+    }
+
+    return () => {
+      //Component Unmount
+      if (socket) {
+        socket.emit("leaveRoom", {
+          chatroomId,
+        });
+      }
+    };
+    //eslint-disable-next-line
+  }, []);
+ 
+  return (
+    <div className="chatroomPage">
+      <div className="chatroomSection">
+        <div className="cardHeader">Chatroom Name</div>
+        <div className="chatroomContent">
+          {messages.map((message, i) => (
+            <div key={i} className="message">
+              <h2
+                // className={
+                //   userId === message.userId ? "ownMessage" : "otherMessage"
+                // }
+              >
+                {message.name}:
+              </h2>{" "}
+              {message.message}
+            </div>
+          ))}
+        </div>
+        <div className="chatroomActions">
+          <div>
+            <input
+              type="text"
+              name="message"
+              placeholder="Say something!"
+              ref={messageRef}
+            />
+          </div>
+          <div>
+            <button className="join" onClick={sendMessage}>
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(ChatroomPage);
